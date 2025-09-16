32b75bcc1fc8b17cd55879fba5bdff7675a74faf
YammD123 <siamqueen52@gmail.com>
2025-09-16 21:44:22 +0700

    Ignore target folder

--- aw_custom_response/src/main.rs
+++ aw_custom_response/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, web};
+use aw_custom_response::Response;
+use sqlx::mysql::MySqlPoolOptions;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let response = Response::<()> {
+        status: "success".to_string(),
+        message: "berhasil".to_string(),
+        data: None,
+    };
+    HttpResponse::Ok().json(response)
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = match MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect("mysql://root:@localhost:3306/1")
+        .await
+    {
+        Err(err) => {
+            eprint!("error koneksi {}", err)
+        }
+        Ok(_) => {
+            println!("Koneksi berhasil")
+        }
+    };
+    HttpServer::new(move || App::new().app_data(web::Data::new(pool)).service(index))
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     HttpServer::new(|| App::new().service(index))
+//         .bind(("127.0.0.1", 8080))?
+//         .run()
+//         .await
+// }
